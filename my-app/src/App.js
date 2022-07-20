7e13753132375b7891b880e604a73c63e57e6890
Fawzaan <fawzi20112001@gmail.com>
2022-07-21 01:34:12 +0530

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+let name = "Fawzaan"; // This is a variable which can then be used in the JSX code using {}
 
 function App() {
   // JSX is returned and all of it has to be enclosed within one tag (could be div or an empty <>...</> tag)
   return (
     // Called as a JSX fragment for letting multiple elements be returned
     <> 
-    <div className="blank">
+    {/* <div className="blank">
       <h1>This is practice</h1>
     </div>
 
@@ -25,6 +27,17 @@ function App() {
           Learn React with Fawzaan
         </a>
       </header>
+    </div> */}
+
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+
+    <h1>Hello {name}</h1>
+    <div className="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti temporibus in perspiciatis sequi maiores, ratione fugiat, labore reprehenderit autem aspernatur nisi dolorem ipsa quidem laudantium sed sunt modi ab, ullam esse libero. Magni, corrupti.
     </div>
     </>
   );
