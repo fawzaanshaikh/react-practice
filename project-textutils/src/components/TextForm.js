02a7e37086884a4cf10e74cf2e68402e23b05d9b
Fawzaan <fawzi20112001@gmail.com>
2022-08-07 15:37:40 +0530

    useState hook used for TextForm Component state

--- project-textutils/src/components/TextForm.js
+++ project-textutils/src/components/TextForm.js
@@ -1,15 +1,17 @@
 import React from 'react'
+// import React, {useState} from 'react'; {useState} is known as a hook
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
   return (
     <>
+        const[text, setText] = useState("Enter text here"); {/* This is used to track the change in our component's state in terms of the change in text, this uses array destructuring */}
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <label for="myBox" className="form-label">Example textarea</label>
+            <label htmlFor="myBox" className="form-label">Example textarea</label>
             <textarea className="form-control" id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary"></button>
+        <button className="btn btn-primary">Convert to Uppercase</button>
     </>
   )
 }
