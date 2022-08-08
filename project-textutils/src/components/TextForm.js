d4718ecafc07b5add285e07abb744ecc0fd2caab
Fawzaan <fawzi20112001@gmail.com>
2022-08-09 00:12:14 +0530

    Update to word and character count

--- project-textutils/src/components/TextForm.js
+++ project-textutils/src/components/TextForm.js
@@ -13,16 +13,10 @@ export default function TextForm(props) {
         setText(event.target.value);    // This allows the text to be written from user input in addition to the text that is there in the state variable 'text'
     }
 
-    const countWords = () => {
-        setWordCount();
-    }
-
     // This is used to track the change in our component's state in terms of the change in text, this uses array destructuring
     const [text, setText] = useState("Enter text here");
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
-
-    const [numberOfWords, setWordCount] = useState("Number of words are = 0");
 
   return (
     <>
@@ -31,13 +25,12 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>&nbsp;&nbsp;&nbsp;
-            <button className="btn btn-primary" onClick={countWords}>Count the Number of Words</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
 
-        <div className="container">
+        <div className="container my-5">
             <h1>Your text summary</h1>
-            <p>{numberOfWords}</p>
+            <p>Number of words = {text.split(" ").length} and characters = {text.length}</p>
         </div>
     </>
   )
