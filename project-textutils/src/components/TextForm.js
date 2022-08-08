22e105577b6403e2badf9a4115819cb6df12c7a2
Fawzaan <fawzi20112001@gmail.com>
2022-08-08 23:42:40 +0530

    Number of words button added

--- project-textutils/src/components/TextForm.js
+++ project-textutils/src/components/TextForm.js
@@ -13,17 +13,32 @@ export default function TextForm(props) {
         setText(event.target.value);    // This allows the text to be written from user input in addition to the text that is there in the state variable 'text'
     }
 
+    const countWords = () => {
+        setWordCount();
+    }
+
     // This is used to track the change in our component's state in terms of the change in text, this uses array destructuring
     const [text, setText] = useState("Enter text here");
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
+
+    const [numberOfWords, setWordCount] = useState("Number of words are = 0");
+
   return (
     <>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+        <div className="container">
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>&nbsp;&nbsp;&nbsp;
+            <button className="btn btn-primary" onClick={countWords}>Count the Number of Words</button>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+
+        <div className="container">
+            <h1>Your text summary</h1>
+            <p>{numberOfWords}</p>
+        </div>
     </>
   )
 }
