9dfb66e78a2ae159b6ea985542ea38f86cc86504
Fawzaan <fawzi20112001@gmail.com>
2022-09-22 23:49:57 +0530

    Removed Cumulative Layout Shift due to alerts

--- project-textutils/src/components/Alert.js
+++ project-textutils/src/components/Alert.js
@@ -7,11 +7,14 @@ function Alert(props) {
     }
 
   return (
-    // props.alert part is evaluated first. IF it is null, then the entire expression is false, and hence, the remaining expression is not calculated, resulting in an error to not be declared
-    props.alert && <div>
+    // this is done to remove the Cumulative Layout Shift and set a height value for the alert message
+    <div style={{height: '50px'}}> 
+    {/* props.alert part is evaluated first. IF it is null, then the entire expression is false, and hence, the remaining expression is not calculated, resulting in an error to not be declared */}
+    {props.alert && <div>
         <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{capitalize(props.alert.type)}</strong>: {props.alert.message}
         </div>
+    </div>}
     </div>
   )
 }
