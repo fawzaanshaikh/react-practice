4052e0b5934dc9672c5ab55f4a8239b3d462c415
Fawzaan <fawzi20112001@gmail.com>
2022-10-20 14:46:31 +0530

    Update

--- misc/src/App.js
+++ misc/src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const shortenWidth = {
-    width: "wrap-content"
-  };
-
-  const greyBackground = {
-    color: "grey"
-  };
   
   return (
     <>
       <div className="container text-center">
-        <div className="container-within row text-center my-5 white-bg">
-          <div className="col text-center my-5">Module 1</div>
+        <div className="container-within row text-center my-5">
+          <div className="col text-center white-bg"  id="module-banner"><span id="banner-text">Module Banner</span></div>
+          <div className="col text-center white-bg" id="badge"><span id="badge-text">C4 Badge</span></div>
         </div>
-        <div className="container-within row text-center my-5 white-bg">
-          <div className="col text-center my-5">Module 2</div>
+
+        <div className="line">-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</div>
+        
+        <div className="row text-center my-5">
+          <div className="col text-center mx-5 white-bg videos"><span className="video-text">Video 1<br/>Course Learnings</span></div>
+          <div className="col text-center mx-5 white-bg videos"><span className="video-text">Video 2<br/>Course Outcomes</span></div>
         </div>
-        <div className="container-within row text-center my-5 white-bg">
-          <div className="col text-center my-5">Module 3</div>
+
+        <div className="row text-center my-5 schedule">
+          <div className="col text-center my-5">Schedule your class</div>
         </div>
-        <div className="container-within row text-center my-5 white-bg">
-          <div className="col text-center my-5">Module 4</div>
+
+        <div className="container-within row text-center my-5">
+          <div className="col text-center mx-5 white-bg"><span className="option-text">Resources</span></div>
+          <div className="col text-center mx-5 white-bg"><span className="option-text">Quizzes</span></div>
         </div>
       </div>
     </>
